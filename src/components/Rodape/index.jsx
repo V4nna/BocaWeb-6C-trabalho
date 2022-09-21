73db3d7e19103773fb391751718b1533ded7554c
V4nna <99482221+V4nna@users.noreply.github.com>
2022-09-21 00:17:32 -0300

    Add files via upload

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,9 @@
+export const Rodape = () => {
+    return(
+        <footer>
+            <p> 
+                IFMS Dourados – Trabalho de Frameworks2 – Geovanna Fina Cruz 6C
+            </p>
+        </footer>
+    )
+}
